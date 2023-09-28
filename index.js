b44ffada3ec1bd43d20c7fe4d48889991d54f32c
Emmanuel Baraka <barakaemmanuel413@gmail.com>
2023-09-28 06:33:27 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks() {
+    const hqLocation = 42;
+    const distanceBlocks = Math.abs(50 -42);
+    return distanceBlocks;
+  }
+  function distanceFromHqInFeet(someValue) {
+    distanceFromHqInBlocks(8);
+    const distanceInBlocks = distanceFromHqInBlocks(8);
+    const feetPerBlock = 264;
+    const distanceInFeet = distanceInBlocks * feetPerBlock;
+    return distanceInFeet;
+    
+    
+  }
+  function distanceTravelledInFeet(start, destination) {
+
+    const distance = distanceTravelledInFeet(34, 38);
+    const blockLengthInFeet = 264;
+    const distanceInFeet = (destination - start) * blockLengthInFeet;
+    return distanceInFeet;
+    console.log(distance);
+  }
+  
+  
